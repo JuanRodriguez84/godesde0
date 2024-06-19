d8be3620959c089076c54996c99ac86ec27711b2
jcrodriguez <jcrodriguez@novopayment.com>
2024-06-19 15:11:45 -0500

    primer commit inicio de variables e importacion de paquetes

--- variables/enteras.go
+++ variables/enteras.go
@@ -2,8 +2,15 @@ package variables
 
 import "fmt"
 
-func MuestroEnteros()
-{ 
-	
-}
+func MuestroEnteros() { // metodos no tienen argumentos de retorno
+	var intcomun int
+	intde32 := int32(10)
 
+	intde64 := int64(100)
+
+	// intde64 = intcomun En go int, int32 y int64 a pesar de ser integer son tipos de datos diferemtes y no se puede hacer algo como eso
+
+	fmt.Println("intcomun = ", intcomun)
+	fmt.Println("intde32 = ", intde32)
+	fmt.Println("intde64 = ", intde64)
+}
